5da7739503abc011dbc601b5a7af56cf88952c3e
hyeondoong2 <sallybi@naver.com>
2025-09-08 17:04:07 +0900

    add test api

--- SimpleGame/Renderer.cpp
+++ SimpleGame/Renderer.cpp
@@ -46,6 +46,22 @@ void Renderer::CreateVertexBufferObjects()
 	glGenBuffers(1, &m_VBORect);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBORect);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(rect), rect, GL_STATIC_DRAW);
+
+	// lecture2 - 2025.9.8
+	float test[]
+		=
+	{
+		0.f, 0.f, 0.f, 
+		1.f, 0.f, 0.f, 
+		1.f, 1.f, 0.f, //Triangle1
+
+	};
+
+	GLuint testID;
+	glGenBuffers(1, &m_VBOTest);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOTest);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(test), test, GL_STATIC_DRAW);
+
 }
 
 void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -188,6 +204,23 @@ void Renderer::DrawSolidRect(float x, float y, float z, float size, float r, flo
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
+
+void Renderer::DrawTest()
+{
+	//Program select
+	glUseProgram(m_SolidRectShader);
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Trans"), 0, 0, 0, 1);
+	glUniform4f(glGetUniformLocation(m_SolidRectShader, "u_Color"), 1, 1, 1, 1);
+	int attribPosition = glGetAttribLocation(m_SolidRectShader, "a_Position");
+	glEnableVertexAttribArray(attribPosition);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOTest);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT,
+		GL_FALSE, sizeof(float) * 3, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(attribPosition);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
 
 void Renderer::GetGLPosition(float x, float y, float *newX, float *newY)
 {
